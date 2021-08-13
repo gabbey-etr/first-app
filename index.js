c9633acab0f82432550e1fbe879dd661b77046d6
gabbey-etr <eleuteriogabriel98@hotmail.com>
2021-08-13 20:09:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require ("express");
 const app = express();
+const routes = require("./routes");
 
 const port=3000;
 const address="localhost";
 //criação do servidor com node e express
+
+app.use('/',routes);
 
 const server = app.listen(port,address,()=>{
     let host = server.address().address;
